cecf31701d149d280873492bc9899105bdbf585c
Dmytro-Mykhailenko <daemonkt@gmail.com>
2024-02-03 11:38:20 +0200

    Homework #3 about increment in return.

--- src/NumberManipulator.java
+++ src/NumberManipulator.java
@@ -1,13 +1,13 @@
 public class NumberManipulator {
     public int incrementByOne(int number){
         // return number++; так изменения не передавались
-        number++;
-        return number;
+
+        return number++;
     }
     public int decrementByOne(int number){
         // return number--; так изменения не передавались
-        number--;
-        return number;
+
+        return number--;
     }
 
 }
